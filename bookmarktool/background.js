dbdafed0c9e0c11bdd50f9ed1cc552363f3482b0
ej8899 <ej8899@gmail.com>
2023-01-20 21:29:31 -0500

    refactor: update extension to manifest v3

--- bookmarktool/background.js
+++ bookmarktool/background.js
@@ -1,5 +1,7 @@
 chrome.action.onClicked.addListener(function(tab) {
   var tabUrl = tab.url;
-  // Send the tab URL to your localhost server
-  alert(tabUrl)
+
+  // Send the tab URL to your localhost server:
+  // can do automatically here, or let it occur in popup after a secondary click to allow person to categorize it w a dropdown list first perhaps
+  
 });
