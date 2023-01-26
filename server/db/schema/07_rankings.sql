63de174ed0657f43f2860a0d872af27322a45989
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-26 04:00:44 +0000

    ci: create rankings table

---
+++ server/db/schema/07_rankings.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS rankings CASCADE;
+
+CREATE TABLE rankings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  profile_id INTEGER REFERENCES profiles(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  note VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
