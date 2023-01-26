ca61d21ed0a0d55941e3927960ee3778de17e0b1
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-26 03:47:11 +0000

    ci: create comments table

---
+++ server/db/schema/06_comments.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS comments CASCADE;
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  profile_id INTEGER REFERENCES profiles(id) ON DELETE CASCADE,
+  comment_id INTEGER REFERENCES comments(id) DEFAULT NULL ON DELETE CASCADE,
+  comment VARCHAR(2000) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
