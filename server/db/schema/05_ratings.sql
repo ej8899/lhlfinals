129c17c22b2f8e8204b3cd131a01657db08aa078
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-26 03:30:45 +0000

    ci: create ratings table

---
+++ server/db/schema/05_ratings.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS ratings CASCADE;
+
+CREATE TABLE ratings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  profile_id INTEGER REFERENCES profiles(id) ON DELETE CASCADE,
+  rate INT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
