830a93c28b11cc986b846bc9c7e1c34e20ce99a8
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-26 02:55:37 +0000

    ci: create profiles table

---
+++ server/db/schema/02_profiles.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS profiles CASCADE;
+
+CREATE TABLE profiles (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
