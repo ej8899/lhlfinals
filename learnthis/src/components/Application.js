110c54669919ae6372d5d262b14fbed030e714a8
ej8899 <ej8899@gmail.com>
2023-01-27 07:15:53 -0500

    fix: add boundary box to masonry to force columns

--- learnthis/src/components/Application.js
+++ learnthis/src/components/Application.js
@@ -153,7 +153,7 @@ export default function Application(props) {
 
         <main className={className} id={theme}>
 
-        <Box sx={{ width: 1000, minHeight: 377 }}>
+        <Box sx={{ width: 1400, minHeight: 377 }}>
         <Masonry columns={4} spacing={2}>
         {sampledata.map((item, index) => (
           <PreviewItem key={index} videoId={{ item }}>
