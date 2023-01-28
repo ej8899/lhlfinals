8701359e0a9682af18ba4b3124ffa72f8f0ed4b3
ej8899 <ej8899@gmail.com>
2023-01-28 10:01:57 -0500

    ci: update hero framework

--- learnthis/src/components/Hero/Hero.jsx
+++ learnthis/src/components/Hero/Hero.jsx
@@ -1,6 +1,20 @@
-import { grey } from "@mui/material/colors";
+import { Container, Grid, Typography,Button } from "@mui/material";
+
 import "../Application.scss";
 import SpringModal from "../ItemDetail/index.jsx";
+
+
+const heroContainer = {
+    paddingTop: "64px",
+    border: "1px solid red"
+  };
+const heroContent =  {
+    textAlign: "center",
+    padding: "32px",
+    border: "1px solid red"
+  };
+
+
 
 export default function Hero(props) {
   function showAbout() {
@@ -8,19 +22,26 @@ export default function Hero(props) {
   }
   
   return (
-    <div style={{marginTop: '40px',
-                backgroundColor: '#616161'}}>
-      <br/><br/>
-    hero location: <br/><br/> <a className="socicons">
-                  <i
-                    onClick={() => showAbout()}
-                    className="fa-solid fa-circle-question fa-xl"
-                  ></i>
-                </a> | cookies | 
-                <br/><br/>
 
-    <SpringModal></SpringModal>
-    </div>
+    <Container maxWidth="xl" style={heroContainer}>
+    <Grid container>
+      <Grid item xs={12}>
+        <div style={heroContent}>
+          <Typography variant="h1">Welcome to our site!</Typography>
+          <Typography variant="subtitle1">
+          hero location: <br/><br/> <a className="socicons">
+                      <i
+                        onClick={() => showAbout()}
+                        className="fa-solid fa-circle-question fa-xl"
+                      ></i>
+                    </a> | cookies | put other 'chips' or buttons here for quick sort of primary categories
+          </Typography>
+          <Button variant="contained" color="secondary">Learn More</Button>
+        </div>
+      </Grid>
+    </Grid>
+    </Container>
+
   );
 };
 
