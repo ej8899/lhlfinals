10ea32003cabbbf0e04e63640e9ed11aa35dba59
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-29 00:26:32 -0800

    Add userRoute to the server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "..", "learnthis/build");
+
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET_KEY]
+}));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const userDatabase = require('./db/userDatabase');
+const userRoutes = require('./routes/userRoutes');
+
+// /user/endpoints
+const userRouter = express.Router();
+userRoutes(userRouter, userDatabase);
+app.use('/api/user', userRouter);
 
 app.use(express.static(publicPath));
 app.get("/*", function (req, res) {
