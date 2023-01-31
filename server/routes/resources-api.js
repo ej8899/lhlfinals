fc974b003ea85fc61518aeb3fdb50f33af949d6e
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-31 15:58:52 +0000

    ci: implement POST api/resources

--- server/routes/resources-api.js
+++ server/routes/resources-api.js
@@ -2,23 +2,42 @@
  * All routes for resources created by all users are defined here
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const q_resources = require('../db/queries/q_resources');
+const q_resources = require("../db/queries/q_resources");
 
 /**
-* Get all resources comming from all users that are still active from db
-* @return {json} All resources in db that are not deleted limit by 20.
-*/
-router.get('/', (req, res) => {
-  q_resources.getAllResources()
-    .then(data => res.json(data))
+ * Get all resources comming from all users that are still active from db
+ * @return {json} All resources in db that are not deleted limit by 20.
+ */
+router.get("/", (req, res) => {
+  q_resources
+    .getAllResources()
+    .then((data) => res.json(data))
     .catch((err) => {
-      res
-        .status(500)
-        .json({ error: err.message });
+      res.status(500).json({ error: err.message });
     });
 });
 
+/**
+ * Check if a user exists with a given username and password
+ * @return {json} resource that is saved
+ * 
+ */
+router.post("/", (req, res) => {
+  //const userId = req.session.userID;
+  const resourceData = req.body;
+
+  q_resources
+    .postResource(resouceData)
+    .then((data) => {
+      console.log("Resouce save returned obj: ", data);
+      return data;
+    })
+    .catch((err) => {
+      console.log("Error saving new resource", err);
+      return res.status(500).json({ error: err.message });
+    });
+});
 
 module.exports = router;
