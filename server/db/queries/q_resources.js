7f8e747b1822eb7024aed697a91292e10dcc8532
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-31 13:34:38 +0000

    ci: implement q_resources/getAllResources

---
+++ server/db/queries/q_resources.js
@@ -0,0 +1,17 @@
+const db = require('../connection');
+
+/**
+* Get all resources comming from all users that are still active from db
+* @return {Promise<{}>} A promise of all resources in db that are not deleted limit by 20.
+*/
+const getAllResources = () => {
+  const params = ['NOT NULL']
+  return db.query('SELECT * FROM resources WHERE deleted_at = $1 ORDER BY id LIMIT 20;',params)
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = {
+  getAllResources
+}
