dc56f17c5aaccef92635e865f2d7e1d035c4151e
ej8899 <ej8899@gmail.com>
2023-01-31 22:05:54 -0500

    ci: style web clipper

--- bookmarktool/popup.js
+++ bookmarktool/popup.js
@@ -1,13 +1,49 @@
+//
+// LearnThis! Resource Web Clipper
+//
+
+// process submit
 function getCurrentTabUrl() {
-  var queryInfo = {
+  let queryInfo = {
     active: true,
     currentWindow: true,
   }
 
+  // grab resource category
+  let select = document.getElementById("options");
+  let selectedOption = select.options[select.selectedIndex].value;
+
+  // grab any supplied note information
+  let note = document.getElementById("note");
+  let noteText = note.value;
+
+  // grab any supplied page title information
+  let pageTitle = document.getElementById("page-title");
+  let pageTitleText = pageTitle.value;
+
+  // grab resource url from current tab
   chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0]
-    alert(tab.url)
+    let tab = tabs[0];
+    alert(tab.url + " - category - " + selectedOption + " - notes - " + noteText + " - page title - " + pageTitleText);
   })
 }
 
-document.querySelector('#get-url').addEventListener('click', getCurrentTabUrl)
+
+// call chrome tabs API
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  let currentTab = tabs[0];
+  //alert("The title of the current tab is: " + currentTab.title);
+  
+  // set the default resource title to current page title
+  document.getElementById("page-title").value = currentTab.title;
+});
+
+// listen form form submission
+document.querySelector('#get-url').addEventListener('click', getCurrentTabUrl);
+
+// listen for close button
+let closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", function() {
+  window.close();
+});
+
