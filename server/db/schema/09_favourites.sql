d17e943b776e7cc5690d4b74917491c1a6ffa59a
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-02-06 22:35:31 +0000

    ci: create seeds/favourite

--- server/db/schema/09_favourites.sql
+++ server/db/schema/09_favourites.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS favourites CASCADE;
 
 CREATE TABLE favourites (
   id SERIAL PRIMARY KEY NOT NULL,
-  resource_id INTEGER REFERENCES resource_id(id) ON DELETE CASCADE,
-  profile_id INTEGER REFERENCES profile_id(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  profile_id INTEGER REFERENCES profiles(id) ON DELETE CASCADE,
   is_favourite BOOLEAN,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP,
