933b45d0209efae9fbeba242f4cb6e16b584b50c
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-02-08 13:22:40 -0800

    Create the post resource with addition query

--- server/db/queries/q_resources.js
+++ server/db/queries/q_resources.js
@@ -1,5 +1,5 @@
 const db = require("../connection");
-
+const { postResourcesQueryHelper } = require("../../helper/query");
 /**
  * Get all resources comming from all users that are still active from db
  * @return {Promise<{}>} A promise of all resources in db that are not deleted limit by 20.
@@ -22,21 +22,20 @@ const getAllResources = () => {
  * Get all resources with addition of likes, categories, rankings and ratings
  * @return {Promise<{}>} A promise of all resources in db that are not deleted limit by 20.
  */
-const getAllResourcesWithAddition = () => {
-  //const params = ["NOT NULL"];
-  return db
-    .query(
-      `SELECT res.*, COUNT(DISTINCT l.id) AS likes, array[c.name] AS categories, AVG(ran.SCALE) AS ranking, AVG(rat.rate) AS rating
+const getAllResourcesWithAddition = (id = undefined) => {
+  const whereClause = id === undefined ? 'WHERE res.deleted_at IS NULL' : `WHERE res.id=${id} AND res.deleted_at IS NULL`;
+  const query = `SELECT res.*, COUNT(DISTINCT l.id) AS total_likes, array[c.name] AS categories, AVG(ran.SCALE) AS avg_ranking, AVG(rat.rate) AS avg_rating
     FROM resources AS res
     LEFT JOIN likes AS l on res.id=l.resource_id
     LEFT JOIN categories AS c on res.id=c.resource_id
     LEFT JOIN rankings AS ran on res.id=ran.resource_id
     LEFT JOIN ratings AS rat on res.id=rat.resource_id
-    WHERE res.deleted_at IS NULL
+    ${whereClause}
     GROUP BY res.id, c.name
-    ORDER BY res.id LIMIT 20;`
-    )
-    .then((data) => {
+    ORDER BY res.id LIMIT 20;`;
+
+  return db
+    .query(query).then((data) => {
       if (data.rows.length === 0) {
         return [];
       }
@@ -45,7 +44,9 @@ const getAllResourcesWithAddition = () => {
       data.rows.forEach(resource => {
         if (resources.map(r => r.id).includes(resource.id)) {
           const index = resources.findIndex(r => r.id === resource.id);
-          resources[index] = { ...resources[index], categories: resources[index].categories.concat(resource.categories) };
+          resources[index] = {
+            ...resources[index],
+            categories: resources[index].categories.concat(resource.categories) };
         } else {
           resources.push(resource);
         }
@@ -53,7 +54,6 @@ const getAllResourcesWithAddition = () => {
       return resources;
     });
 };
-
 /**
  * Insert new resource
  * @param {json} resource data
@@ -80,6 +80,59 @@ const postResource = (data) => {
   ];
 
   return db.query(query, params).then((data) => data.rows[0]);
+};
+
+/**
+ * Insert new resource with addition
+ * @param {json} resource data
+ * @return {Promise<{}>} A promise of the resource inserted.
+ */
+const postResourceWithAddition = async (data) => {
+  let resourceQuery = `
+  INSERT INTO
+    resources (
+      profile_id,
+      url,
+      title,
+      description,
+      thumbnail
+    )
+    VALUES
+      ($1, $2, $3, $4, $5) RETURNING *;`;
+  const resourceParams = [
+    data.resource.profile_id,
+    data.resource.url,
+    data.resource.title,
+    data.resource.description,
+    data.resource.thumbnail,
+  ];
+
+  try {
+    const savedData = {};
+    await db.query('BEGIN');
+    const resource = await db.query(resourceQuery, resourceParams).then((data) => data.rows[0]);
+    savedData['resource'] = resource;
+    const resourceWithAddition = await getAllResourcesWithAddition(resource.id);
+    savedData['resource'] = {
+      ...savedData['resource'],
+      'categories': resourceWithAddition['categories'],
+      'avg_rating': resourceWithAddition['avg_raging'],
+      'avg_ranking': resourceWithAddition['avg_ranking'],
+      'total_likes': resourceWithAddition['total_likes'],
+    };
+    if (data.user) {
+      const postHelper = postResourcesQueryHelper(data, resource.id);
+      for (helper of postHelper) {
+        await db.query(helper.query, helper.params).then((data) => data.rows[0]);
+      }
+      savedData['user'] = data.user;
+    }
+    await db.query('COMMIT');
+    return savedData;
+  } catch (err) {
+    await db.query('ROLLBACK')
+    throw err;
+  }
 };
 
 /**
@@ -134,6 +187,7 @@ module.exports = {
   getAllResources,
   getAllResourcesWithAddition,
   postResource,
+  postResourceWithAddition,
   updateResource,
   deleteResource
 };
