94f347cf5e42c5f2d59a236c888eb828e8f7384d
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-02-08 14:41:02 +0000

    ci: create schema/reports

---
+++ server/db/schema/13_reports.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS reports CASCADE;
+
+CREATE TABLE reports (
+  id SERIAL PRIMARY KEY NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  profile_id INTEGER REFERENCES profiles(id) ON DELETE CASCADE,
+  is_reported BOOLEAN,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
